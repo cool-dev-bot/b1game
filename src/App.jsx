f6682092b5d5790d2ed8655fab28140cf6391cb8
neonblack-01 <jayy.codage@gmail.com>
2024-09-05 23:18:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,53 @@ import './App.css';
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
 import Loader from './components/Loader.jsx'; // Import the Loader component
+import { loginContext } from '../context/context.jsx';
+import axios from 'axios';
 
 function App() {
   const [loginUser, setLoginUser] = useState(false);
   const [depositPopUp, setDespositPopUp] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [saveUserInfo, setSaveUserInfo] = useState(JSON.parse(localStorage.getItem('userInfo')))
+  const [userDetails, setUserDetails] = useState({})
+
+  async function handleGetUser() {
+    try {
+      // Set up the headers with the Bearer token
+      const config = {
+        headers: {
+          Authorization: `Bearer ${saveUserInfo}`
+        }
+      };
+      // Make the request with the configured headers
+      const res = await axios.get(`https://roulette-backend-pearl.vercel.app/api/users/me`, config);
+      // Log the response or handle it as needed
+      setUserDetails(res.data);
+    } catch (error) {
+      // Handle any errors that occur during the request
+      console.error('Error fetching user data:', error);
+    }
+  }
 
   useEffect(() => {
     // Simulate a loading delay (e.g., fetching data, waiting for assets to load)
     setTimeout(() => {
       setLoading(false);
-    }, 1000); // Adjust the time as needed
-  }, []);
+    }, 1000);
+
+    handleGetUser()
+  }, [saveUserInfo]);
 
   if (loading) {
     return <Loader />;
   }
 
   return (
-    <div className='mainBg max-w-screen h-full mx-auto'>
+    <loginContext.Provider value={{ setSaveUserInfo, userDetails }} className='mainBg max-w-screen h-full mx-auto'>
       <Header setLoginUser={setLoginUser} loginUser={loginUser} setDespositPopUp={setDespositPopUp} depositPopUp={depositPopUp} />
       <Outlet context={[setLoginUser, loginUser, setDespositPopUp, depositPopUp]} />
       <Footer />
-    </div>
+    </loginContext.Provider>
   );
 }
 
